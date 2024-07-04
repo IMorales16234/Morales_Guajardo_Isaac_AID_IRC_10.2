00098d0118d271fa15f33c6c330e94ece87a5d77
IMorales16234 <156528757+IMorales16234@users.noreply.github.com>
2024-07-04 13:20:20 -0600

    Dockers

---
+++ segundo_docker/app/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def home():
+   return "Mi segundo Docker"
+
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
